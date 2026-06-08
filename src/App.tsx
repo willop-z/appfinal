931c53f4e963de17c2369003c78cd99914c780c1
Wilmer Garcia <garcialopezwilmer@gmail.com>
2026-06-08 13:43:53 -0500

    se configura app.tsx para manejar el inicio de sesión con un ternario

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,22 @@ import { useState } from 'react'
 import { IonApp } from '@ionic/react'
 import '@ionic/react/css/core.css'
 import Login from './pages/Login'
+import Home from './pages/Home'
 
 function App() {
   const [usuario, setUsuario] = useState('')
-  const [pantallaActual, setPantallaActual] = useState<'home' | 'backhome'>('home')
+  
 
   return (
     <IonApp>
       { usuario === '' ? (
       <Login onLogin={setUsuario} /> )
       : (usuario === 'wilmer' || usuario === 'nicolas' || usuario === 'camilo') ? (
-          pantallaActual === 'home' ? (
         <Home
           usuario={usuario}
           onLogout={() => setUsuario('')}
-          onBackhome={() => setPantallaActual('backhome')}
         /> )
         : (
-        <Backhome // cada portafolio
-          usuario={usuario}
-          onVolver={() => setPantallaActual('home')}
-          onLogout={() => setUsuario('')}
-        />
-      ) )
-      : (
           <div className="pantalla-error">
             <h2>Error de Acceso</h2>
             <p>El usuario "{usuario}" no está autorizado.</p>
