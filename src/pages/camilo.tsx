83b91b06b64ffae8e5a977acac58b4628765d763
Camilo <Camilloo222@gmail.com>
2026-06-08 13:15:38 -0500

    Trabajo de camilo

---
+++ src/pages/camilo.tsx
@@ -0,0 +1,72 @@
+import {
+  IonPage,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonButton
+} from '@ionic/react'
+
+import '/camilo.css'
+
+interface Props {
+  usuario: string
+  onLogout: () => void
+}
+
+function Home({ usuario, onLogout }: Props) {
+  return (
+    <IonPage>
+      <IonContent className="ion-padding fondo-home">
+
+        <h1 className="titulo-home">
+          Bienvenido a mi espacio personal
+        </h1>
+
+        <IonCard className="card-home">
+
+          <IonCardHeader>
+            <IonCardTitle>
+              Hola, {usuario} 👋
+            </IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+
+            <p className="texto-home">
+              Este es mi proyecto personal desarrollado con Ionic React.
+            </p>
+
+            <h3>Sobre mí</h3>
+
+            <ul className="lista-home">
+              <li>Me gusta escuchar musica</li>
+              <li>💻 Apasionado por la programación</li>
+              <li>🌐 Interesado en redes y desarrollo web</li>
+              <li>Disfruto ver peliculas de accion y terror</li>
+            </ul>
+
+            <p>
+              Gracias por visitar mi aplicación.
+            </p>
+
+            <IonButton
+              color="danger"
+              expand="block"
+              className="boton-logout"
+              onClick={onLogout}
+            >
+              Cerrar sesión
+            </IonButton>
+
+          </IonCardContent>
+
+        </IonCard>
+
+      </IonContent>
+    </IonPage>
+  )
+}
+
+export default Home
